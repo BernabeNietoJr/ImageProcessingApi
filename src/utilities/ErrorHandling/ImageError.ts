d2fcb14e89a329f1b9b1fcc7d52ba36ed1457856
Bernabe Nieto Jr <bernabe@MacBook-Air.hitronhub.home>
2021-06-08 15:11:19 -0500

    1st commit with all dependencies installed

--- src/utilities/ErrorHandling/ImageError.ts
+++ src/utilities/ErrorHandling/ImageError.ts
@@ -1,9 +1,9 @@
-class ImageApiError extends Error{
+class ImageApiError extends Error {
 
     constructor(message: string, status: number) {
         super();
         this.message = message;
-        //this.status = status;
+        // --this.statusCode = status;
     }
 }
 
